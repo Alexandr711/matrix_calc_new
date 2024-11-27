2469037411c81b0861fd71965e34a9fef4432e2b
Alexandr711 <65649872+Alexandr711@users.noreply.github.com>
2024-11-27 19:35:19 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -17,3 +17,34 @@ Matrix::Matrix(int str, int cols): numberString(str), numberCols(cols)
         }
     }
 }
+
+Matrix Matrix::operator+(Matrix matrixOne)
+{
+  Matrix tempMatrix(numberStr, numberCols);
+  for(int i = 0; i < numberStr; ++i)
+    {
+    for(int j = 0; j < numberCols; ++j)
+      {
+        tempMatrix.matrix[i][j] = matrixOne.matrix[i][j] + matrix[i][j];
+      }
+    }
+  return tempMatrix;
+  }
+
+Matrix Matrix::operator-(Matrix matrixOne)
+{
+  Matrix tempMatrix(numberStr, numberCols);
+  for(int i = 0; i < numberStr; ++i)
+    {
+      for(int j = 0; j < numberCols; ++j)
+        {
+          tempMatrix.matrix[i][j] = matrix[i][j] - matrixOne[i][j];
+        }
+    }
+  return tempMatrix;
+}
+
+
+
+
+
