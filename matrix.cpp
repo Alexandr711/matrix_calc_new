3e9b2d9ba54f923061d2e8a8ec017c7ea39d5b1c
Alexandr711 <65649872+Alexandr711@users.noreply.github.com>
2024-11-27 18:52:32 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,13 @@
+#include "matrix.hpp"
 
+Matrix::Matrix()
+{
+  numberString = 0;
+  numberCols = 0;
+  matrix = nullptr;
+}
+
+Matrix::Matrix(int str, int cols): numberString(str), numberCols(cols)
+{
+  matrix = 
+}
